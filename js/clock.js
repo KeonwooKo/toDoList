69dbdc11345584e30005727590546c25adf53bc3
Phil Ko <35305115+KeonwooKo@users.noreply.github.com>
2022-07-04 22:28:17 +0900

    update with my style

--- js/clock.js
+++ js/clock.js
@@ -1,13 +1,43 @@
 const clock = document.querySelector("h2#clock");
 
-function getClock(){
-    const date = new Date();
-    const hours = String(date.getHours()).padStart(2, "0");
-    const minutes = String(date.getMinutes()).padStart(2, "0");
-    const seconds = String(date.getSeconds()).padStart(2, "0");
-    
-    clock.innerText = `${hours}:${minutes}:${seconds}`;
+function getClock() {
+  var day_array = new Array(
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+  );
+  var month_array = new Array(
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+  );
+  const today = new Date();
+  const hours = String(today.getHours()).padStart(2, "0");
+  const minutes = String(today.getMinutes()).padStart(2, "0");
+  const seconds = String(today.getSeconds()).padStart(2, "0");
+  const year = today.getFullYear();
+  const month = today.getMonth() + 1;
+  const month_label = month_array[month];
+  const day = today.getDay();
+  const day_label = day_array[day];
+  const date = today.getDate();
+
+  clock.innerText = `${year} ${month_label},${date} ${day_label}`;
+  // clock.innerText = `${hours}:${minutes}:${seconds}`;
 }
 
-getClock()
+getClock();
 setInterval(getClock, 1000);
--- js/greetings.js
+++ js/greetings.js
@@ -2,27 +2,27 @@ const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
 
-const HIDDEN_CLASSNAME = "hidden"
-const USERNAME_KEY = "username"
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
 
-function onLoginSubmit(event){
-    event.preventDefault();
-    const username = loginInput.value;
-    loginForm.classList.add(HIDDEN_CLASSNAME);
-    localStorage.setItem(USERNAME_KEY, username)
-    paintGreeting(username)
+function onLoginSubmit(event) {
+  event.preventDefault();
+  const username = loginInput.value;
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+  localStorage.setItem(USERNAME_KEY, username);
+  paintGreeting(username);
 }
 
-function paintGreeting(username){
-    greeting.classList.remove(HIDDEN_CLASSNAME);
-    greeting.innerText = `Hello ${username}`
+function paintGreeting(username) {
+  greeting.classList.remove(HIDDEN_CLASSNAME);
+  greeting.innerText = `${username}'s Todo-List`;
 }
 
 const savedUsername = localStorage.getItem(USERNAME_KEY);
 
-if(savedUsername === null){
-    loginForm.classList.remove(HIDDEN_CLASSNAME);
-    loginForm.addEventListener("submit", onLoginSubmit);
-}else {
-    paintGreeting(savedUsername)
+if (savedUsername === null) {
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+  paintGreeting(savedUsername);
 }
